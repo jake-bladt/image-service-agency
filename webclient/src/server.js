4f7e8b14821dad90357d9778f6b14c3b24fd10d6
Jake Bladt <jake@jakebladt.com>
2016-12-20 20:49:04 -0500

    allow for port configuration

--- webclient/src/server.js
+++ webclient/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.IMGSVCA_WEBCLIENT_PORT || 3000;
 
 app.use(express.static('client'));
 app.listen(3000);
